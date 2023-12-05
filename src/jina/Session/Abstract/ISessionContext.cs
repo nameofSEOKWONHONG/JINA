35926da0bb3df4d47b229e261729e2213d2dc640
h20913 <h20913@gmail.com>
2023-12-05 23:59:56 +0900

    update

--- src/jina/Session/Abstract/ISessionContext.cs
+++ src/jina/Session/Abstract/ISessionContext.cs
@@ -2,15 +2,17 @@
 
 public interface ISessionContext
 {
-    public string TenantId { get; }
+    string TenantId { get; }
 
     /// <summary>
     /// Token 정보 중 사용자 정보
     /// </summary>
-    public ISessionCurrentUser CurrentUser { get; }
+    ISessionCurrentUser CurrentUser { get; }
 
     /// <summary>
     /// UTC 타임 정보
     /// </summary>
-    public ISessionDateTime CurrentTime { get; }
+    ISessionDateTime CurrentTime { get; }
+
+    bool IsDecrypt { get; }
 }
