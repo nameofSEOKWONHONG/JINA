2c15258371ab5c740e133ea787329ada646b5362
h20913 <h20913@gmail.com>
2024-01-03 00:57:55 +0900

    update

--- src/Jina.Lang/ENUM_LOCALE_TYPE.cs
+++ src/Jina.Lang/ENUM_LOCALE_TYPE.cs
@@ -1,26 +1,31 @@
+using Ardalis.SmartEnum;
 using eXtensionSharp;
 
 namespace Jina.Lang;
 
-public class ENUM_LOCALE_TYPE : XEnumBase<ENUM_LOCALE_TYPE>
+public class ENUM_LOCALE_TYPE : SmartEnum<ENUM_LOCALE_TYPE>
 {
     /// <summary>
     /// 영어
     /// </summary>
-    public static ENUM_LOCALE_TYPE EN_US = Define("en-US");
+    public static ENUM_LOCALE_TYPE EN_US = new ENUM_LOCALE_TYPE("en-US", 1);
     
     /// <summary>
     /// 한국어
     /// </summary>
-    public static ENUM_LOCALE_TYPE KO_KR = Define("ko-KR");
+    public static ENUM_LOCALE_TYPE KO_KR = new ENUM_LOCALE_TYPE("ko-KR", 2);
     
     /// <summary>
     /// 중국어-간체
     /// </summary>
-    public static ENUM_LOCALE_TYPE ZH_CN = Define("zh-CN");
+    public static ENUM_LOCALE_TYPE ZH_CN = new ENUM_LOCALE_TYPE("zh-CN", 2);
     
     /// <summary>
     /// 일본어
     /// </summary>
-    public static ENUM_LOCALE_TYPE JA_JP = Define("ja-JP");
+    public static ENUM_LOCALE_TYPE JA_JP = new ENUM_LOCALE_TYPE("ja-JP", 2);
+
+    public ENUM_LOCALE_TYPE(string name, int value) : base(name, value)
+    {
+    }
 }
