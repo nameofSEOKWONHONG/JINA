b3f72446223659a03d858b5fe56b609756cec47d
seokwonhong <h20913@gmail.com>
2024-02-20 11:17:18 +0900

    add closexml wrapper

---
+++ src/Jina.Excel/ExcelWrapper.cs
@@ -0,0 +1,70 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using eXtensionSharp;
+
+namespace Jina.Excel
+{
+    public class ExcelWrapper
+    {
+        private readonly ExcelWrapperCore _excelWrapperCore;
+        public ExcelWrapper()
+        {
+            _excelWrapperCore = new ExcelWrapperCore();
+        }
+
+        /// <summary>
+        /// Create excel
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="sheetName"></param>
+        /// <param name="datum"></param>
+        /// <param name="worksheetHandler">after create, modify style, mergem etc</param>
+        public void CreateExcel(string filePath, string sheetName, List<DynamicDictionary<string>> datum, Action<IXLWorksheet> worksheetHandler = null)
+        {
+            using var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add(sheetName);
+
+            _excelWrapperCore.CreateExcelCore(ws, datum, worksheetHandler);
+
+            workbook.SaveAs(filePath);
+        }
+
+        /// <summary>
+        /// Create excel to byte array
+        /// </summary>
+        /// <param name="sheetName"></param>
+        /// <param name="datum"></param>
+        /// <param name="worksheetHandler">after create, modify style, mergem etc</param>
+        /// <returns></returns>
+        public byte[] CreateExcel(string sheetName, List<DynamicDictionary<string>> datum, Action<IXLWorksheet> worksheetHandler = null)
+        {
+            using var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add(sheetName);
+
+            _excelWrapperCore.CreateExcelCore(ws, datum, worksheetHandler);
+
+            using var stream = new MemoryStream();
+            workbook.SaveAs(stream);
+            return stream.ToArray();
+        }
+        
+        /// <summary>
+        /// Manually crate excel
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="sheetName"></param>
+        /// <param name="worksheetHandler"></param>
+        public void CreateExcel(string filePath, string sheetName, Action<IXLWorksheet> worksheetHandler)
+        {
+            using var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add(sheetName);
+
+            if(worksheetHandler.xIsNotEmpty()) 
+            {
+                worksheetHandler(ws);
+            }
+
+            workbook.SaveAs(filePath);
+        }
+    }
+}
---
+++ src/Jina.Excel/ExcelWrapperCore.cs
@@ -0,0 +1,46 @@
+﻿using ClosedXML.Excel;
+using eXtensionSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jina.Excel
+{
+    internal class ExcelWrapperCore
+    {
+        public ExcelWrapperCore()
+        {
+            
+        }
+
+        public void CreateExcelCore(IXLWorksheet ws, List<DynamicDictionary<string>> datum, Action<IXLWorksheet> worksheetHandler = null)
+        {
+            var rowIdx = 1;
+            var headers = datum.xFirst().Keys.ToArray();
+            for (var i = 0; i < headers.Length; i++)
+            {
+                var header = headers[i];
+                ws.Cell(rowIdx, (i + 1)).Value = header;
+            }
+            rowIdx++;
+
+            foreach (var data in datum)
+            {
+                var colIdx = 1;
+                foreach (var header in headers)
+                {
+                    ws.Cell(rowIdx, colIdx).Value = data[header];
+                    colIdx++;
+                }
+                rowIdx++;
+            }
+
+            if (worksheetHandler.xIsNotEmpty())
+            {
+                worksheetHandler(ws);
+            }
+        }
+    }
+}
