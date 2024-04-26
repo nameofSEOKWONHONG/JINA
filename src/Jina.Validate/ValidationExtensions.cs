e247fc4d0ef0172c10caf83aa4c9bee539564c3a
seokwonhong <h20913@gmail.com>
2024-04-26 19:58:08 +0900

    update

--- src/Jina.Validate/ValidationExtensions.cs
+++ src/Jina.Validate/ValidationExtensions.cs
@@ -1,4 +1,8 @@
-﻿using FluentValidation.Results;
+﻿using System.Reflection;
+using eXtensionSharp;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Jina.Validate;
 
@@ -8,5 +12,20 @@ public static class ValidationExtensions
     {
         return items.Select(m => new KeyValuePair<string, string>(key: m.PropertyName, value: m.ErrorMessage))
             .ToDictionary();
+    }
+    
+    public static IServiceCollection AddValidator(this IServiceCollection services, string domainName,
+        string extensionName = ".dll")
+    {
+        var files = Directory.GetFiles(AppContext.BaseDirectory)
+            .Where(m => Path.GetFileName(m).Contains(domainName) && Path.GetExtension(m) == extensionName)
+            .ToList();
+        var assemblies = new List<Assembly>();
+        files.xForEach(file =>
+        {
+            assemblies.Add(Assembly.Load(Path.GetFileNameWithoutExtension(file)));
+        });
+        services.AddValidatorsFromAssemblies(assemblies);
+        return services;
     }
 }
